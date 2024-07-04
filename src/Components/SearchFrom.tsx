a32f0f6d4dfce565cae0227654b91ff399bd5a6c
salvatoreDeploy <liderhenrique@gmail.com>
2024-07-03 23:18:21 -0300

    envs client e server side

--- src/Components/SearchFrom.tsx
+++ src/Components/SearchFrom.tsx
@@ -31,7 +31,7 @@ export function SearchForm() {
       onSubmit={handleSearch}
       className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700"
     >
-      <Suspense>
+      <Suspense fallback={null}>
         <Search className="size-5 text-zinc-500" />
       </Suspense>
 
